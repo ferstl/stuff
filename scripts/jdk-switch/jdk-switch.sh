8809c11f3a6adf7b246d39c7f7a85141a57c55ba
Stefan Ferstl <st.ferstl@gmail.com>
2021-12-27 16:52:06 +0100

    Add JDK 17

--- scripts/jdk-switch/jdk-switch.sh
+++ scripts/jdk-switch/jdk-switch.sh
@@ -12,8 +12,9 @@ JDK11=zulu-11
 JDK13=zulu-13
 JDK15=zulu-15
 JDK16=zulu-16
+JDK17=zulu-17
 
-DEFAULT_JDK="$JDK16"
+DEFAULT_JDK="$JDK17"
 
 # Switch the JDK to the given verison
 # The switch will set JAVA_HOME and add it to the PATH
